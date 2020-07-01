89b0fb8891bfd56e2ac3f59988c4af6801df5142
Clement <juanito.tanjung@tiket.com>
2020-07-01 18:45:11 +0700

    map view

--- routes/Map/index.jsx
+++ routes/Map/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions, Image } from 'react-native';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 
 export default function Map() {
 
@@ -25,41 +25,41 @@ export default function Map() {
         zoomEnabled={false}
         provider={PROVIDER_GOOGLE}
       >
-        {/*<Marker*/}
-        {/*  // image={require("../../assets/dog.png")}*/}
-        {/*  draggable*/}
-        {/*  coordinate={{*/}
-        {/*    latitude: -6.104490,*/}
-        {/*    longitude: 106.795030,*/}
-        {/*  }}*/}
-        {/*  onDragEnd={(e) => alert(JSON.stringify(e.nativeEvent.coordinate))}*/}
-        {/*  title={'Pet A'}*/}
-        {/*  description={'This is a description of the marker'}*/}
-        {/*>*/}
-        {/*  <Image source={require("../../assets/dog.png")} style={{height: 50, width: 50 }} />*/}
-        {/*</Marker>*/}
+        <Marker
+          // image={require("../../assets/dog.png")}
+          draggable
+          coordinate={{
+            latitude: -6.104490,
+            longitude: 106.795030,
+          }}
+          onDragEnd={(e) => alert(JSON.stringify(e.nativeEvent.coordinate))}
+          title={'Pet A'}
+          description={'This is a description of the marker'}
+        >
+          <Image source={require("../../assets/dog.png")} style={{height: 50, width: 50 }} />
+        </Marker>
 
-        {/*<Marker*/}
-        {/*  coordinate={{*/}
-        {/*    latitude: -6.104490,*/}
-        {/*    longitude: 106.796030,*/}
-        {/*  }}*/}
-        {/*  title={'Pet B'}*/}
-        {/*  description={'This is a description of the marker'}*/}
-        {/*>*/}
-        {/*  <Image source={require("../../assets/circle-color.png")} style={{height: 150, width: 150 }} />*/}
-        {/*</Marker>*/}
+        <Marker
+          coordinate={{
+            latitude: -6.104490,
+            longitude: 106.796030,
+          }}
+          title={'Pet B'}
+          description={'This is a description of the marker'}
+        >
+          <Image source={require("../../assets/circle-color.png")} style={{height: 150, width: 150 }} />
+        </Marker>
 
-        {/*<Marker*/}
-        {/*  draggable*/}
-        {/*  coordinate={{*/}
-        {/*    latitude: -6.104490,*/}
-        {/*    longitude: 106.792030,*/}
-        {/*  }}*/}
-        {/*  onDragEnd={(e) => alert(JSON.stringify(e.nativeEvent.coordinate))}*/}
-        {/*  title={'Pet C'}*/}
-        {/*  description={'This is a description of the marker'}*/}
-        {/*/>*/}
+        <Marker
+          draggable
+          coordinate={{
+            latitude: -6.104490,
+            longitude: 106.792030,
+          }}
+          onDragEnd={(e) => alert(JSON.stringify(e.nativeEvent.coordinate))}
+          title={'Pet C'}
+          description={'This is a description of the marker'}
+        />
       </MapView>
     </View>
   );
